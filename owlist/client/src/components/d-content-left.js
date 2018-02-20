58efca85b1ce11c79d3830e2d9af765d4474d8cb
Jennifer Marcum <Jen@jennifermarcum.com>
2018-02-19 22:43:56 -0800

    Updated Files

--- owlist/client/src/components/d-content-left.js
+++ owlist/client/src/components/d-content-left.js
@@ -13,24 +13,13 @@ class ContentLeft extends Component {
 	render(){
 	return(
 		<section id="main-content-left" className="col mt-4">
-		{/*
-		ContentLeft will default to TaskList when user logs in.
-
-		If Notifications is clicked on the top nav
-		then it will display <NotificationsList /> 
-
-		If Calendar is clicked in the taskbar
-		then it will display <GoogleCalendar />
-		*/}
 		
 		<Switch>
-		<Route exact path="/dashboard/tasks" component={TaskList}/>
+		<Route exact path="/dashboard" component={TaskList}/>
         <Route exact path="/dashboard/notifications" component={NotificationsList}/>
         <Route exact path="/dashboard/calendar" component={GoogleCalendar}/>
        	</Switch>
 
-	
-		
 		</section>
 	);
 };
--- owlist/client/src/components/d-taskbar.js
+++ owlist/client/src/components/d-taskbar.js
@@ -20,9 +20,9 @@ class TaskBar extends React.Component {
 			<h3 className="dk-blue-header section-header pt-3">{this.state.ProjectName}</h3>
 			<ul id="task-nav">
 				{/*My Tasks li needs to display contentleft/TaskList*/}
-				<li><NavLink to="dashboard/tasks" className="active">My Tasks</NavLink></li>
+				<li><NavLink exact to="/dashboard">My Tasks</NavLink></li>
 				{/*Calendar li needs to display contentleft/GoogleCalendar*/}
-				<li><NavLink to="dashboard/calendar" >Calendar</NavLink></li>
+				<li><NavLink to="/dashboard/calendar" >Calendar</NavLink></li>
 			</ul>
 		</section>
 		);
